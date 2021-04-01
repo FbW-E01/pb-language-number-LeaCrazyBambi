1134844e2291020b45c003fa1aff5a2b0822ca10
LeaCrazyBambi <lheynitz@gmail.com>
2021-04-01 11:14:20 +0200

    Like this?

---
+++ numbers.js
@@ -0,0 +1,15 @@
+//  1. Concatenation
+let introSentence = "hello, my name is Fran and I am ";
+let age = 25;
+console.log(introSentence + age);
+// What is the + operator doing? 
+// It is adding both variables ...
+
+//  2. Converting
+const numberOne = "1005";
+console.log(typeof numberOne);
+console.log(parseInt(numberOne));
+console.log(typeof parseInt(numberOne));
+
+const numberTwo = "10.05";
+console.log(typeof parseFloat (numberTwo));
